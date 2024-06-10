827484fbbb5170388d6e93942ec784fd45e6dd2d
Jihan Jasper Al-rashid <69488696+j-jasper-a@users.noreply.github.com>
2024-06-10 11:54:53 +0600

    fix: fix header z-index

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import TopBar from "./TopBar";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 z-20 w-full bg-dark">
+    <header className="fixed top-0 z-30 w-full bg-dark">
       <div className="mx-auto flex max-w-screen-2xl flex-col">
         <TopBar />
         <NavigationBar />
