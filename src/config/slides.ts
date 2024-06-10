cd4590b976e50045eae69a5c2dba0aaf6bf7c245
Jihan Jasper Al-rashid <69488696+j-jasper-a@users.noreply.github.com>
2024-06-10 08:50:26 +0600

    chore: reset hero

---
+++ src/config/slides.ts
@@ -0,0 +1,23 @@
+export type Slide = {
+  image: string;
+  heading: string;
+  subheading: string;
+};
+
+export const slides: Slide[] = [
+  {
+    image: "/assets/images/hero/new-arrivals.webp",
+    heading: "New Arrivals",
+    subheading: "The newest and latest advances on technology",
+  },
+  {
+    image: "/assets/images/hero/picture-perfect-deals.webp",
+    heading: "Picture Perfect Deals",
+    subheading: "Browse our second-hand gear market",
+  },
+  {
+    image: "/assets/images/hero/make-believe.webp",
+    heading: "Make Believe",
+    subheading: "Sales on selected SONY products",
+  },
+];
